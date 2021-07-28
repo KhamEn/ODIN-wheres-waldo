08fbda9b6702615a82b63a5d3ab73c05fd5e47c9
Kham En <khamen28@gmail.com>
2021-07-28 11:07:00 -0400

    Toggle character selection menu on mouse click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { useState } from "react";
 import PhotoTagger from "./components/PhotoTagger";
+import CharacterSelectionMenu from "./components/CharacterSelectionMenu";
+import "./App.css";
 
 function App() {
+  const [menuIsOn, setMenusIsOn] = useState(false);
+
+  function handleOnClick() {
+    menuIsOn ? setMenusIsOn(false) : setMenusIsOn(true);
+  }
+
   return (
-    <div className="App">
-      <PhotoTagger/>
+    <div className="App" onClick={handleOnClick}>
+      {menuIsOn && <CharacterSelectionMenu />}
+      <PhotoTagger />
     </div>
   );
 }
---
+++ src/components/CharacterSelectionMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CharacterSelectionMenu() {
+    return (
+        <ul>
+            <li>Liara</li>
+            <li>Talia</li>
+            <li>Garrus</li>
+        </ul>
+    );
+}
+
+export default CharacterSelectionMenu;
